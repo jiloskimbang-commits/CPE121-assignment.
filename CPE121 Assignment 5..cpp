a0afc0c6cee797d7a86c98337372f9ae1fc6c061
Jiloski <jiloskimbang@gmail.com>
2026-07-06 06:15:17 +0100

    Add files via upload
    This is the file for CPE121 assignment, number 5.

---
+++ CPE121 Assignment 5..cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Row, row, row your boat" << endl;
+	cout << "Gently down the stream" << endl;
+	cout << "Merily, merily, merily, merily" << endl;
+	cout << "Life is but a dream" << endl;
+	return 0;
+}
