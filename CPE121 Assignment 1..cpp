9a9e88e1c8b71f692c2e6b64a7b7d5b3ef4044c5
Jiloski <jiloskimbang@gmail.com>
2026-07-06 06:04:42 +0100

    Add files via upload
    This is the file for CPE121 assignment, number 1.

---
+++ CPE121 Assignment 1..cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+	cout << "MBANG, JIL SUNDAY" << endl;
+	
+	cout << "25/EG/CO/027" << endl;
+	
+	cout << "COMPUTER ENGINEERING" << endl;
+	
+	return 0;
+}
