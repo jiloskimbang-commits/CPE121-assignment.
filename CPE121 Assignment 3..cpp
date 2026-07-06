86c945fcaceb43213e4075b5e40764577e345645
Jiloski <jiloskimbang@gmail.com>
2026-07-06 06:08:59 +0100

    Add files via upload
    This is the file for CPE121 assignment, number 3.

---
+++ CPE121 Assignment 3..cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello, World!" << endl;
+	cout << "Welcome to C++ Programming." << endl;
+	cout << "Department of Computer Engineering" << endl;
+	cout << "University of Uyo" << endl;
+	cout << "2025/2026 Academic Session" << endl;
+	return 0;
+}
